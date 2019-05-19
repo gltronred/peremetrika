343a34eb4d0a43bfca5fba9577756dd51266df8c
Mansur Ziiatdinov <gltronred@gmail.com>
2019-05-19 09:31:03 +0300

    Скрипт конвертирования в json работает как пайп

--- scripts/fcd-xml-to-json.sh
+++ scripts/fcd-xml-to-json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
- xq '.["fcd-export"].timestep | map({ time: .["@time"], vehicles: (if .vehicle then [.vehicle] | flatten | map ({ x: .["@x"], y: .["@y"], angle: .["@angle"], type: .["@type"]}) else [] end) })' $1
+xq '.["fcd-export"].timestep | map({ time: .["@time"], vehicles: (if .vehicle then [.vehicle] | flatten | map ({ x: .["@x"], y: .["@y"], angle: .["@angle"], type: .["@type"]}) else [] end) })'
 
